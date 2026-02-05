285eaa63fac9911d9a3683932d97131d44ceb007
Amar Bapic <59930522+abapic94@users.noreply.github.com>
2026-02-05 16:28:49 +0100

    Deployed f6c97da with MkDocs version: 1.6.1

---
+++ js/analytics.js
@@ -0,0 +1,5 @@
+var s = document.createElement("script");
+s.src = "https://gc.zgo.at/count.js";
+s.setAttribute("data-goatcounter", "https://booleaner-mathe.goatcounter.com/count");
+s.async = true;
+document.head.appendChild(s);
